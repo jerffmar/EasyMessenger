70bddc24aaf213010e56c6307533743aab2f1099
jerffson marinheiro <jerffson.marinheiro@gmail.com>
2026-02-06 09:47:55 -0300

    modified:   server/routes/auth.ts

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { initializeAuth, validatePassword } from '../services/auth';
+import { initializeAuth, validatePassword } from '../services/auth.js';
 
 const router = express.Router();
 
